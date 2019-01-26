49fa3cb897c6e3713820935341c4b2d0ef8afaf7
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-01-26 20:48:30 +0530

    Create set9.7.c

---
+++ set9.7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,x,gcd,lcm;
+clrscr();
+printf("enter two value:");
+scanf("%d%d",&a,&b);
+c=a;
+d=b;
+while(b!=0)
+{
+x=b;
+b=a%b;
+a=x;
+}
+gcd=a;
+lcm=(c*d)/gcd;
+printf("\ngcd is %d",gcd);
+getch();
+}
