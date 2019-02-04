90475179ea59c14f74719efcc118eac4786f0c6b
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-04 22:29:05 +0530

    Create set9.10.c

---
+++ set9.10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>='0' && a[i]<='9')
+        {
+          printf("%c",a[i]);  
+        }
+    }
+    return 0;
+}
