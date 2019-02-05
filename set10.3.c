432b671861ecaa5ac170aee4a3a6ce0c8d257a42
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-05 18:52:44 +0530

    Create set10.3.c

---
+++ set10.3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int degree,deg;
+clrscr();
+printf("enter the degree");
+scanf("%d",&degree);
+deg=(degree-32)*5/9;
+printf("celsius is %d",deg);
+getch();
+}
