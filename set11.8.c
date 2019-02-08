bf62a0d8cc4da470c55c2c6a563daddc0443d4aa
vidhya <46036915+nobilotus@users.noreply.github.com>
2019-02-08 18:47:54 +0530

    Create set11.8.c

---
+++ set11.8.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,i,j,k,c,z,max,pos;
+clrscr();
+printf("no of value and k value");
+scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+printf("hai%d\n",max);
+ c=max;
+for(i=0;i<k;i++)
+{
+for(j=0;j<n;j++)
+  {
+  if(a[j]<c)
+  {
+  c=a[j];
+  pos=j;
+  z=j;
+  }
+  }
+  a[pos]=a[pos]+max;
+  c=a[pos];
+  }
+ printf("min is %d",a[z]-max);
+ getch();
+}
